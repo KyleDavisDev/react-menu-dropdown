03b1f2605d4a378b45375bb0906c05140c89f682
Kyle Bonar <kylewb2011@gmail.com>
2019-02-03 11:37:55 -0600

    init commit

---
+++ src/components/Trigger/Trigger.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+interface TriggerProps {
+  children:
+    | string
+    | Element
+    | JSX.Element
+    | Array<string | Element | JSX.Element>;
+  className?: string;
+}
+
+const Trigger: React.SFC<TriggerProps> = props => {
+  return <a className={props.className}>{props.children}</a>;
+};
+
+export default Trigger;
